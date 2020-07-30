3ebce0ff8bb423598efa5265a6b7c91058260a3e
Nimitha-jagadeesha <nimitha1jagadeesha@gmail.com>
2020-07-31 01:52:16 +0530

    .

--- app/src/main/java/com/example/covidcare/activities/AdminOptions.java
+++ app/src/main/java/com/example/covidcare/activities/AdminOptions.java
@@ -2,18 +2,48 @@ package com.example.covidcare.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import com.example.covidcare.R;
-public class AdminOptions extends AppCompatActivity {
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.example.covidcare.R;
+
+import java.util.ArrayList;
+
+public class AdminOptions extends AppCompatActivity {
+    Spinner stateSelectSpinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_options);
         bindViews();
+
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("JAVA");
+        arrayList.add("ANDROID");
+        arrayList.add("C Language");
+        arrayList.add("CPP Language");
+        arrayList.add("Go Language");
+        arrayList.add("AVN SYSTEMS");
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, arrayList);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        stateSelectSpinner.setAdapter(arrayAdapter);
+        stateSelectSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String tutorialsName = parent.getItemAtPosition(position).toString();
+                Toast.makeText(parent.getContext(), "Selected: " + tutorialsName, Toast.LENGTH_LONG).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView <?> parent) {
+            }
+        });
     }
 
     private void bindViews() {
-
+    stateSelectSpinner =findViewById(R.id.AdminOptionsStatesSpinner);
     }
 
 }
