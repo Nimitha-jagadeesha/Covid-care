9ab0fd431c0570e3f93da00d3d207ec2aa1428fe
Nimitha-jagadeesha <nimitha1jagadeesha@gmail.com>
2020-07-30 12:33:51 +0530

    Login And signIn

--- app/src/main/java/com/example/covidcare/MainActivity.java
+++ app/src/main/java/com/example/covidcare/MainActivity.java
@@ -4,10 +4,13 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -78,18 +81,29 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         int id = item.getItemId();
 
         if (id == R.id.logout) {
-            Toast.makeText(this, "Toast", Toast.LENGTH_LONG).show();
-        } else if (id == R.id.about) {
-
+            FirebaseAuth.getInstance().signOut();
+            finish();
+            startActivity(new Intent(this,SignInOrRegister.class));
+        }
+        else if (id == R.id.nav_share) {
+            Intent sendIntent = new Intent();
+            sendIntent.setAction(Intent.ACTION_SEND);
+            sendIntent.putExtra(Intent.EXTRA_TEXT,
+                    "Hey check out the app at: https://play.google.com/store/apps/details?id=" + BuildConfig.APPLICATION_ID);
+            sendIntent.setType("text/plain");
+            startActivity(sendIntent);
 
 
         } else if (id == R.id.emergencyCall) {
 
-
-
-        } else if (id == R.id.map) {
-
-
+            String s = "tel:1075" ;
+            Intent intent = new Intent(Intent.ACTION_CALL);
+            intent.setData(Uri.parse(s));
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE},1);
+                return true;
+            }
+            startActivity(intent);
 
         }
 
