33e9bd1f3f6b9d5fac17fd57ed057b6c790104e1
Nimitha-jagadeesha <nimitha1jagadeesha@gmail.com>
2020-07-31 21:46:57 +0530

    Basic structure of HospitalBed Info

--- app/src/main/java/com/example/covidcare/models/HospitalData.java
+++ app/src/main/java/com/example/covidcare/models/HospitalData.java
@@ -2,23 +2,27 @@ package com.example.covidcare.models;
 
 public class HospitalData {
     String numberOfBeds;
-    String HospitalName;
+    String hospitalName;
+    String region;
 
     public HospitalData() {
     }
 
-
-    public HospitalData(String numberOfBeds, String hospitalName) {
+    public HospitalData(String numberOfBeds, String hospitalName, String region) {
         this.numberOfBeds = numberOfBeds;
-        HospitalName = hospitalName;
+        this.hospitalName = hospitalName;
+        this.region = region;
     }
 
+    public String getRegion() {
+        return region;
+    }
 
     public String getNumberOfBeds() {
         return numberOfBeds;
     }
 
     public String getHospitalName() {
-        return HospitalName;
+        return hospitalName;
     }
 }
