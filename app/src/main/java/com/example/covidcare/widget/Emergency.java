79029d16876bfbaedbb4de7ba9aebef1e3972e10
Nimitha-jagadeesha <nimitha1jagadeesha@gmail.com>
2020-08-02 23:33:05 +0530

    Added widget

---
+++ app/src/main/java/com/example/covidcare/widget/Emergency.java
@@ -0,0 +1,64 @@
+package com.example.covidcare.widget;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.widget.RemoteViews;
+
+import com.example.covidcare.R;
+import com.example.covidcare.activities.MainActivity;
+
+import androidx.core.app.ActivityCompat;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class Emergency extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.emergency);
+        views.setTextViewText(R.id.appwidget_text, widgetText);
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+
+        
+        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.emergency);
+        String s = "tel:1075";
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse(s));
+        PendingIntent configPendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+        remoteViews.setOnClickPendingIntent(R.id.widget, configPendingIntent);
+        appWidgetManager.updateAppWidget(appWidgetIds, remoteViews);
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
